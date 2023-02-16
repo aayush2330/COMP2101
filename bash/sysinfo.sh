2ea894bad40724af6e923016e632debcef96e756
Aayush Shrestha <aayushshrestha2330@gmail.com>
2023-02-15 21:39:29 -0500

    assignment completed

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-echo "FQDN: $(hostname)"  #(hostname) is replaced by the fully qualified domain name (FQDN) of the current system
-echo "Host Information:"
-hostnamectl status        #To check the information about the hostname configuration on a system
-echo "IP Adresseses:"
-hostname -I               #to display ip addresses pf the current system
-echo "Root file status:" 
-df /run                   # to check the disk space usage of the file system installed  on the /run directory.
 
+hostname=$(hostname)
+OSNAME=$(lsb_release -ds)
+IP=$(ip route | grep default | cut -d ' ' -f3)
+ROOTSPACE=$(df --output=avail -h / | tail -n1)
 
+cat <<EOF
 
+Report for my$hostname
+==================
+FQDN: $hostname
+Operating System name and version: $OSNAME
+IP Address: $IP
+Root Filesystem Free Space: $ROOTSPACE
+==================
+
+EOF
